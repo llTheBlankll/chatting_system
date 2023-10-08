8b975832164c1c4cab932f6effed5fa99b628d02
Neltri <llTheBlankll@gmail.com>
2023-10-09 03:22:37 +0800

    Main page initial design

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import "./main.scss"
+import { HeaderComponent } from "./components/Header";
+import { Chatlist } from "./components/Chatlist";
+import { Chat } from "./components/Chat";
 
 function MainPage() {
     return (
-        <h1>test</h1>
+        <div className="container">
+            <HeaderComponent title="Chatting System" />
+            <div className="container-row" style={{ border: "1px solid red" }}>
+                <Chatlist users={[
+                    {
+                        id: "1",
+                        name: "Vincent Batecan",
+                        avatar: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50",
+                        status: "5 minute ago."
+                    }
+                ]} />
+                <Chat chats={{ chatHistory: ["Hello"], currentChat: "Hello"}} user={{ id: "1", name: "Vincent Batecan", avatar: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50", status: "5 minute ago." }} time={"5 minute ago."} isTyping={true} />
+            </div>
+        </div>
     );
 }
 
